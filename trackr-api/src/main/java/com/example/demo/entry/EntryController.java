d14ae83b2909d37234997d76e2d13b504b467057
jesslyw <jeslyw@gmail.com>
2025-08-08 23:07:41 +0200

    fix docker port issues and mount frontend directory into container

--- trackr-api/src/main/java/com/example/demo/entry/EntryController.java
+++ trackr-api/src/main/java/com/example/demo/entry/EntryController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/v1/entry")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EntryController {
 
     private final EntryRepository entryRepository;
